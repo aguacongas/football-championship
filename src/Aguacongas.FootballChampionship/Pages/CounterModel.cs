995ba98c0d2df8bb3c9209801ea003cd9dae9668
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2019-05-18 14:33:34 +0200

    build(amplify): setup auth

---
+++ src/Aguacongas.FootballChampionship/Pages/CounterModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Aguacongas.FootballChampionship.Pages
+{
+    public class CounterMode: ComponentBase
+    {
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
